cd8c3f5c456a3acfd56b94e60c1697bcf23eaf13
Cemal Ege Özdemir <c.egeozdemir@gmail.com>
2016-12-11 18:06:34 +0300

    npm projesi eklendi

---
+++ main.js
@@ -0,0 +1,10 @@
+var express = require( 'express' );
+var app = express();
+
+app.get( '/', function ( req, res ) {
+  res.send( 'Merhaba' );
+} );
+
+app.listen( 3000, function () {
+  console.log( 'Sunucu baslatildi...' );
+} );
